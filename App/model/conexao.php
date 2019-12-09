31e7f877248f816f4477dba48f3e7c6c574d8609
Matuzael <matuzaeldias@hotmail.com>
2019-12-08 21:32:41 -0300

    remote mysql

--- App/model/conexao.php
+++ App/model/conexao.php
@@ -9,8 +9,8 @@ class Conexao{
 
         //Se não existir instância, crie uma
         if(!isset(self::$instance)):
-            self::$instance = new \PDO('mysql:host=localhost;dbname=controle_vendas;charset=utf8','root','');
-        
+            //self::$instance = new \PDO('mysql:host=localhost;dbname=controle_vendas;charset=utf8','root','');
+            self::$instance = new \PDO('mysql:host=remotemysql.com;dbname=Cv9ubDbscf;charset=utf8','Cv9ubDbscf','J4d8qoMGZa');
         endif;
 
         return self::$instance;
