703bc2b8aa4ee7b1c01e5880b489cf8ce401b7f5
Matuzael <matuzaeldias@hotmail.com>
2019-12-09 02:10:22 -0300

    test

--- App/inicio.php
+++ App/inicio.php
@@ -1,6 +1,6 @@
 <?php 
   session_start();
-require_once "../vendor/autoload.php";
+    require_once "../vendor/autoload.php";
 
   if(!isset($_SESSION['nome'])):
     header("Location: login.php");
